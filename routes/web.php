5faadf58735d16eb9841291921ae7f5165125d8e
Moe Kahn <jajamoo@gmail.com>
2024-08-05 23:15:27 -0400

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\StripeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/subscribe', [StripeController::class, 'createSubscription']);
+Route::post('/upgrade', [StripeController::class, 'upgradeSubscription']);
